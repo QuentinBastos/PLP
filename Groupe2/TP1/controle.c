87b73ad87a4569697ea68043f0d1c02e43fd3e81
quentin <quentin.bastos@etu.univ-lyon1.Fr>
2025-11-06 14:57:03 +0100

    feat(ex7): controle

--- Groupe2/TP1/controle.c
+++ Groupe2/TP1/controle.c
@@ -2,25 +2,46 @@
 
 int main(void)
 {
-    const int n = 1000;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i + 1; j++)
-        {
-            if (i == j)
-            {
-                printf("*");
-                printf("\n");
-            }
-            else if (j == 0 || i == n - 1)
-            {
-                printf("*");
-            }
-            else
-            {
-                printf("#");
-            }
-        }
+    printf("Divisible by 4 but not by 6:\n");
+    for (int i = 1; i <= 1000; i++) {
+        if (i % 4 != 0)
+            continue;
+        if (i % 6 == 0)
+            continue;
+        printf("%d ", i);
     }
+    printf("\n\n");
+
+    printf("Even and divisible by 8:\n");
+    int i = 1;
+    while (i <= 1000) {
+        if (i % 2 != 0) {
+            i++;
+            continue;
+        }
+        if (i % 8 == 0) {
+            printf("%d ", i);
+        } else {
+        }
+        if (i == 1000)
+            break;
+        i++;
+    }
+    printf("\n\n");
+
+    printf("Divisible by 5 or 7 but not by 10:\n");
+    i = 1;
+    do {
+        int div5 = i % 5 == 0;
+        int div7 = i % 7 == 0;
+        int div10 = i % 10 == 0;
+        if ((div5 || div7) && !div10) {
+            printf("%d ", i);
+        } else {
+        }
+        i++;
+    } while (i <= 1000);
+    printf("\n");
+
     return 0;
 }
