f17a42ef5fdb559b9fbc10cf2285df4b5d30f224
AdrRnv <adrian.rabian@gmail.com>
2025-11-25 23:28:00 +0100

    push

--- FAIT-PAR-LE-BOUG/TP2/src/sphere.h
+++ FAIT-PAR-LE-BOUG/TP2/src/sphere.h
@@ -1,3 +1,9 @@
+/*
+ * Nom du fichier : sphere.h
+ * Objectif : Déclarations des fonctions pour le calcul de sphère.
+ * Auteurs : Adrian RABIAN, Quentin BASTOS
+ * Commande pour exécuter le fichier : gcc main.c surface.c volume.c -o sphere -lm && ./sphere
+ */
 #ifndef SPHERE_H
 #define SPHERE_H
 
